d4054f8af2194777dac2acb374b29d911e2606d8
Radek <rexis.freshmaker@gmail.com>
2018-01-22 23:49:04 +0100

    reservations

---
+++ inz-opr-front/src/app/services/orlik.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class OrlikService {
+
+  constructor(private http: Http) { }
+
+  getAllOrliks(): Observable<any>{
+    return this.http.get('http://localhost:3000/api/objects')
+      .map(res=>{
+        return res.json();
+      })
+      
+  }
+
+  addOrlik(orlik){
+    console.log(orlik)
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/api/object',orlik)
+        .subscribe()
+}
+
+deleteOrlik(id): Observable<any>{
+  return this.http.delete('http://localhost:3000/api/object/'+id)
+  .map(res => res.json());
+}
+
+updateStatus(orlik){
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  return this.http.put('http://localhost:3000/api/object/'+orlik._id, orlik)
+      .map(res => res.json());
+}
+
+getSportfieldsTypes(){
+  return this.http.get('http://localhost:3000/api/sportsfields/types')
+  .map(res=>{
+    return res.json();
+  })
+}
+
+getDates(){
+  return this.http.get('http://localhost:3000/api/sportsfields/dateTemplate')
+  .map(res=>{
+    return res.json();
+  })
+}
+
+getSportsFields(){
+  return this.http.get('http://localhost:3000/api/sportsfield')
+      .map(res => res.json());
+}
+
+addSportsfieldToObject(id,type){
+  return this.http.put('http://localhost:3000/api/object/'+id+'/sportsfields',type)
+    .map(res=>{
+      console.log(res.json());
+      res.json()
+    });
+}
+addSportsfield(type){
+  return this.http.post('http://localhost:3000/api/sportsfield',type)
+    .map(res=>{
+      console.log('res',res.json())
+      return res.json();
+    })
+}
+deleteSportsfield(id){
+  return this.http.delete('http://localhost:3000/api/sportsfield/'+id)
+    .map(res=>res.json());
+}
+
+}
